e010033f668e29f159845698e6ed04070163c918
jebyrnes <jarrett.byrnes@umb.edu>
2020-09-10 20:29:25 -0400

    Adding 2020 in class code

---
+++ in_class_code/2020/scripts/my_first_r_script.R
@@ -0,0 +1,44 @@
+#'-------------------------------------------
+#'
+#' @title: My first R script for Biol 607
+#' @author: Jarrett Byrnes
+#'
+#' @date: 2009-09-10
+#'
+#' @Changelog:
+#'
+#'-------------------------------------------
+
+
+
+
+# Three mathematical things to show
+# that R works as a calculator
+
+1
+
+1 + 2
+
+1 * 2 + (3*4)
+
+
+# next thing?
+
+3/4
+10 ^ 2
+-1 + 1
+
+# Arithmetic is cool - but what about complex functions?
+sqrt(4)
+log10(10)
+log(5)
+
+?log
+
+log(x = 10, base = 10) #this is the most careful
+log(10, base = 10) #this is more common usage
+log(10, 10) #what does it mean?
+
+??"logarithm"
+
+
